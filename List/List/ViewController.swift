fda7571fe2ad5255b1babe219d7fa6c36cb80496
DaichiObara <b1018173@fun.ac.jp>
2019-06-17 16:02:18 +0900

    Create Project

---
+++ List/List/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  List
+//
+//  Created by Daichi on 2019/06/17.
+//  Copyright © 2019 Daichi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
