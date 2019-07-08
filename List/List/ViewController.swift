a5a4b8a0bc44d6f46fb0a9d8c7be7f03713c8fb9
DaichiObara <b1018173@fun.ac.jp>
2019-07-08 18:07:53 +0900

    Can't display

--- List/List/ViewController.swift
+++ List/List/ViewController.swift
@@ -8,24 +8,68 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+struct User: Codable {
+var login: String
+}
+
+struct GitList {
     
-    let LIST = [""]
+    static func fetchArticle(completion: @escaping([User]) -> Swift.Void) {
+        
+        let url = "https://api.github.com/users"
+        guard var urlComponents = URLComponents(string: url) else {
+            return
+        }
+        
+        let task = URLSession.shared.dataTask(with: urlComponents.url!) { data, response, error in
+            
+            guard let jsonData = data else {
+                return
+            }
+            do {
+                let users = try JSONDecoder().decode([User].self, from: jsonData)
+                print(users[0].login)
+            } catch {
+                print("error")
+                print(error.localizedDescription)
+            }
+        }
+        task.resume()
+    }
+}
+
+class ViewController: UIViewController {
+    
+    private var tableView = UITableView()
+    fileprivate var users: [User] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        setUpTableView: do {
+            tableView.frame = view.frame
+            tableView.dataSource = self
+            view.addSubview(tableView)
+        }
+        
+        GitList.fetchArticle(completion: { (users) in
+            self.users = users
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
+        })
     }
-
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return LIST.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        cell.textLabel!.text = LIST[indexPath.row]
-        return cell
-    }
-
 }
 
+extension ViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .subtitle, reuseIdentifier: "cell")
+        cell.textLabel?.text = users[indexPath.row].login
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return users.count
+    }
+}
